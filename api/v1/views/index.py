d178a346e6de22b88a6390f31c4d037579b375de
Dkazem91 <Dkazemian@gmail.com>
2018-08-20 20:42:53 +0000

    added api status, fixed import lines on index and views/init

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
 # index
+
 @app_views.route('/status', methods=['GET'])
 def getStatus():
     return make_response(jsonify({'status':'OK'}), 200)
