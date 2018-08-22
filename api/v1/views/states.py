165413158b93f1e092ca7a6f5463167a1372bf0d
gjdame <gregoryjdame@gmail.com>
2018-08-22 18:07:24 +0000

    add states style

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+'''
+States request handler
+'''
 from flask import Flask, make_response, request, jsonify, abort
 from api.v1.views import app_views
 from models import storage
 from models.state import State
 from flasgger import swag_from
-# index
+
 
 @app_views.route('/states', methods=['GET', 'POST'])
 def all_states():
@@ -14,13 +17,14 @@ def all_states():
     if request.method == 'POST':
         if not request.json:
             abort(400, 'Not a JSON')
-        if not 'name' in request.json:
+        if 'name' not in request.json:
             abort(400, 'Missing name')
         new_State = State(**request.get_json())
         new_State.save()
         return make_response(jsonify(new_State.to_dict()), 201)
 
-@app_views.route('/states/<state_id>', methods=['GET', 'DELETE','PUT'])
+
+@app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'])
 @swag_from('state.yml')
 def state(state_id):
     '''parameters:
@@ -51,7 +55,7 @@ def state(state_id):
         if not request.json:
             abort(400, "Not a JSON")
         for key, value in request.json.items():
-            if not key in ["id", "created_at", "updated_at"]:
+            if key not in ["id", "created_at", "updated_at"]:
                 setattr(state, key, value)
         state.save()
         return make_response(jsonify(state.to_dict()), 200)
