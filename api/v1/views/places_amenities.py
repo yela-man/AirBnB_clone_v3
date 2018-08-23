014f22635c2d31631b26317052ccbe46c8b8c1e6
Dkazem91 <Dkazemian@gmail.com>
2018-08-23 20:45:36 +0000

    added fs and db

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -8,34 +8,34 @@ from models import storage
 from models.amenity import Amenity
 from os import getenv
 # index
-
-@app_views.route('places/<place_id>/amenities', methods=['GET'])
-def place_allamens(place_id):
-    place = storage.get('Place', place_id)
-    if not place:
-        abort(404)
-
-    return jsonify([amen.to_dict() for amen in place.amenities])
-
-
-@app_views.route('places/<place_id>/amenities/<amenity_id>',
-                 methods=['DELETE', 'POST'])
-def place_amenity(place_id, amenity_id):
-    amen = storage.get('Amenity', amenity_id)
-    place = storage.get('Place', place_id)
-    if not amen or not place:
-        abort(404)
-
-    if request.method == 'DELETE':
-        if amen not in place.amenities:
+if getenv("HBNB_TYPE_STORAGE") == "db" or getenv("HBNB_TYPE_STORAGE") == "fs":
+    @app_views.route('places/<place_id>/amenities', methods=['GET'])
+    def place_allamens(place_id):
+        place = storage.get('Place', place_id)
+        if not place:
             abort(404)
-        storage.delete(amen)
-        storage.save()
-        return jsonify({}), 200
 
-    if request.method == 'POST':
-        if amen not in place.amenities:
-            place.amenities.append(amen)
-            place.save()
+        return jsonify([amen.to_dict() for amen in place.amenities])
+
+
+    @app_views.route('places/<place_id>/amenities/<amenity_id>',
+                     methods=['DELETE', 'POST'])
+    def place_amenity(place_id, amenity_id):
+        amen = storage.get('Amenity', amenity_id)
+        place = storage.get('Place', place_id)
+        if not amen or not place:
+            abort(404)
+
+        if request.method == 'DELETE':
+            if amen not in place.amenities:
+                abort(404)
+            storage.delete(amen)
+            storage.save()
+            return jsonify({}), 200
+
+        if request.method == 'POST':
+            if amen not in place.amenities:
+                place.amenities.append(amen)
+                place.save()
+                return jsonify(amen.to_dict()), 201
             return jsonify(amen.to_dict()), 200
-        return jsonify(amen.to_dict()), 201
